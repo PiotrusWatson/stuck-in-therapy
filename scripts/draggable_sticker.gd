761c6a434fc5c806603a267578a463a9c7d29029
Aral Açıkalın <43911164+aralacikalin@users.noreply.github.com>
2025-11-12 23:02:08 +0200

    preparing for persisting data when sticker is put (#3)

--- scripts/draggable_sticker.gd
+++ scripts/draggable_sticker.gd
@@ -2,14 +2,19 @@ class_name DraggableSticker
 extends TextureRect
 
 @export var dragged_sticker_prefab: PackedScene
+var stciker_resource: StickerData
+
 
 func setTexture(new_texture: Texture2D) -> void:
 	texture = new_texture
 
+func setData(sticker_data: StickerData) -> void:
+	stciker_resource = sticker_data
 
 func _get_drag_data(at_position: Vector2) -> Variant:
 	var data = 1
-	var dragged_sticker = dragged_sticker_prefab.instantiate()
+	var dragged_sticker = dragged_sticker_prefab.instantiate() as DraggedSticker
+	dragged_sticker.setData(stciker_resource)
 	dragged_sticker.texture = texture
 	dragged_sticker.scale = size / texture.get_size()
 	get_tree().root.add_child(dragged_sticker)
