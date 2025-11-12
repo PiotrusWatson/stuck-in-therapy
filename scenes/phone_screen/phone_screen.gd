3b405ee3de8c5e23c7c73eee7d4265da98075b57
Piotrus Watson <piotruswatson@gmail.com>
2025-11-12 20:52:28 +0200

    made box shorter

--- scenes/phone_screen/phone_screen.gd
+++ scenes/phone_screen/phone_screen.gd
@@ -26,6 +26,7 @@ func make_message(text, is_yours):
 	var message: MessageBox = message_box_prefab.instantiate() 
 	messages.add_child(message)
 	message.set_up(text, is_yours)
+	message.grab_focus()
 	return message
 
 
