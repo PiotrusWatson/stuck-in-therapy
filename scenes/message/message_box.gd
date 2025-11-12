a2834c731488f57d9241205b761136bbc93cf165
Piotrus Watson <piotruswatson@gmail.com>
2025-11-13 00:05:25 +0200

    put an awful face in

--- scenes/message/message_box.gd
+++ scenes/message/message_box.gd
@@ -1,25 +1,30 @@
 extends MarginContainer
 class_name MessageBox
 
-@onready var label = $Message
-@onready var panel_container = $PanelContainer
+@onready var label = $HBoxContainer/PanelContainer/Message
+@onready var panel_container = $HBoxContainer/PanelContainer
+@onready var face = $HBoxContainer/Emote
 @export var your_style: StyleBox
 @export var their_style: StyleBox
-@export var smallest_margin: int
-@export var largest_margin: int
+@export var your_smallest_margin: int
+@export var your_largest_margin: int
+@export var their_smallest_margin: int
+@export var their_largest_margin: int 
 
 func set_up(text: String, is_yours: bool):
 	label.text = text
 	if is_yours:
 		panel_container.add_theme_stylebox_override("panel", your_style)
-		add_theme_constant_override("margin_left", largest_margin)
-		add_theme_constant_override("margin_right", smallest_margin)
+		add_theme_constant_override("margin_left", your_largest_margin)
+		add_theme_constant_override("margin_right", your_smallest_margin)
 		label.horizontal_alignment = HorizontalAlignment.HORIZONTAL_ALIGNMENT_RIGHT
+		face.visible = false
 		
 	else:
 		panel_container.add_theme_stylebox_override("panel", their_style)
-		add_theme_constant_override("margin_right", largest_margin)
-		add_theme_constant_override("margin_left", smallest_margin)
+		add_theme_constant_override("margin_right", your_largest_margin)
+		add_theme_constant_override("margin_left", your_smallest_margin)
 		label.horizontal_alignment = HorizontalAlignment.HORIZONTAL_ALIGNMENT_LEFT
+		face.visible = true
 		
 	
