07091a0c6233ffd8203658eaadff6898c39009d8
Piotrus Watson <piotruswatson@gmail.com>
2025-11-14 19:12:39 +0200

    made an awful animation thing

--- scenes/phone_screen/phone_screen.gd
+++ scenes/phone_screen/phone_screen.gd
@@ -2,19 +2,23 @@ extends ColorRect
 
 @export var dialogue_to_use: DialogueResource
 @export var message_box_prefab: PackedScene
+@export var dotted_anim_box_prefab: PackedScene
 @onready var messages = $MessageBackground/MessageScroller/Messages
 @onready var message_timer = $Timers/MessageTimer
+@onready var time_before_typing = $Timers/TimeBeforeTyping
 @onready var message_scroller = $MessageBackground/MessageScroller
 @onready var scrollbar = message_scroller.get_v_scroll_bar()
 signal memory_choice_triggered
 signal response_choice_triggered(responses)
 signal thought_parsed(text)
 var current_line: DialogueLine
+var current_dotted_anim
 func _ready():
 	current_line = await DialogueManager.get_next_dialogue_line(dialogue_to_use)
 	parse_and_make_message(current_line)
 	scrollbar.changed.connect(handle_scrollbar_changed)
 	message_timer.start()
+	time_before_typing.start()
 
 func handle_scrollbar_changed():
 	message_scroller.scroll_vertical = scrollbar.max_value
@@ -43,20 +47,34 @@ func parse_and_make_message(dialogue_line: DialogueLine):
 	if dialogue_line.responses.size() > 0:
 		response_choice_triggered.emit(dialogue_line.responses)
 		message_timer.stop()
+	else:
+		time_before_typing.start()
 	return make_message(text, is_yours)
 	
 func make_message(text, is_yours):
 	var message: MessageBox = message_box_prefab.instantiate() 
 	messages.add_child(message)
 	message.set_up(text, is_yours)
-	message.grab_focus()
 	return message
 
+func make_dotted_anim():
+	current_dotted_anim = dotted_anim_box_prefab.instantiate()
+	messages.add_child(current_dotted_anim)
+	
+func stop_dotted_anim():
+	current_dotted_anim.queue_free()
+	current_dotted_anim = null
 
 func _on_message_timer_timeout() -> void:
+	stop_dotted_anim()
 	current_line = await dialogue_to_use.get_next_dialogue_line(current_line.next_id)
 	if current_line == null:
 		memory_choice_triggered.emit()
 		message_timer.stop()
 		return
 	parse_and_make_message(current_line)
+	
+
+
+func _on_time_before_typing_timeout() -> void:
+	make_dotted_anim()
