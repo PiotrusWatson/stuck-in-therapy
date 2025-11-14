88bd22f22f406c01f2c3ee7fa51e1a3147723969
Piotrus Watson <piotruswatson@gmail.com>
2025-11-14 20:06:37 +0200

    decrunched

--- scenes/message/message_box.gd
+++ scenes/message/message_box.gd
@@ -3,12 +3,14 @@ class_name MessageBox
 
 @onready var message = $HBoxContainer/MessagePanel
 @onready var face = $HBoxContainer/Emote
+@onready var sticker = $HBoxContainer/Sticker
+
 @export var your_smallest_margin: int
 @export var your_largest_margin: int
 @export var their_smallest_margin: int
 @export var their_largest_margin: int 
 
-func set_up(text: String, is_yours: bool):
+func set_up(text: String, is_yours: bool):	
 	if is_yours:
 		add_theme_constant_override("margin_left", your_largest_margin)
 		add_theme_constant_override("margin_right", your_smallest_margin)
@@ -18,5 +20,13 @@ func set_up(text: String, is_yours: bool):
 		add_theme_constant_override("margin_left", their_smallest_margin)
 		face.visible = true
 	message.set_up(text, is_yours)
+	message.visible = true
+	sticker.visible = false
 		
-	
+func set_up_sticker(image: Texture2D):
+	add_theme_constant_override("margin_right", their_largest_margin)
+	add_theme_constant_override("margin_left", their_smallest_margin)
+	face.visible = true
+	message.visible = false
+	sticker.visible = true
+	sticker.texture = image
