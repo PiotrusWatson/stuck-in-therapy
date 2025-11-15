78ee927f639a30a8073e26aa916947f8509e60ea
Piotrus Watson <piotruswatson@gmail.com>
2025-11-15 20:49:04 +0200

    started making something

--- scripts/autoloads/globals.gd
+++ scripts/autoloads/globals.gd
@@ -5,3 +5,7 @@ enum MessageType{MESSAGE, STICKER}
 enum GameState {FIRST_DIALOGUE, HOSPITAL, SECOND_DIALOGUE, GRAVEYARD, THIRD_DIALOGUE, END}
 var state: GameState
 var memory_image
+
+var sticker_dictionary =  {
+	"smile": load("res://")
+}
