d944565a3f32cbadc70ad5c30e930efc993140dd
Aral Acikalin <aralacikalin@hotmail.com>
2025-11-15 17:56:07 +0200

    fix mouse cursor offset

--- worlds/memory.gd
+++ worlds/memory.gd
@@ -22,10 +22,10 @@ func _ready():
 		var resized_dragging_cursor = set_scaled_cursor(dragging_cursor, cursor_size)
 		var resized_non_dragging_cursor = set_scaled_cursor(non_dragging_cursor, cursor_size)
 
-		Input.set_custom_mouse_cursor(resized_dragging_cursor, Input.CURSOR_CAN_DROP)
+		Input.set_custom_mouse_cursor(resized_dragging_cursor, Input.CURSOR_CAN_DROP, Vector2(cursor_size / 2.0, cursor_size / 2.0))
 		# Because scrollbar is forbidden to drop, we set the forbidden cursor when over the scrollbar
-		Input.set_custom_mouse_cursor(resized_dragging_cursor, Input.CURSOR_FORBIDDEN)
-		Input.set_custom_mouse_cursor(resized_non_dragging_cursor, Input.CURSOR_ARROW)
+		Input.set_custom_mouse_cursor(resized_dragging_cursor, Input.CURSOR_FORBIDDEN, Vector2(cursor_size / 2.0, cursor_size / 2.0))
+		Input.set_custom_mouse_cursor(resized_non_dragging_cursor, Input.CURSOR_ARROW, Vector2(cursor_size / 2.0, cursor_size / 2.0))
 
 func _on_exit_button_pressed() -> void:
 	var transition_background_tween = create_tween()
@@ -52,3 +52,11 @@ func set_scaled_cursor(texture: Texture2D, scale: float) -> ImageTexture:
 	var img := texture.get_image()
 	img.resize(cursor_size, cursor_size)
 	return ImageTexture.create_from_image(img)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		# Update cursor based on whether dragging or not
+		if Input.is_action_just_pressed("LeftMouse"):
+			Input.set_custom_mouse_cursor(set_scaled_cursor(dragging_cursor, cursor_size), Input.CURSOR_ARROW,  Vector2(cursor_size / 2.0, cursor_size / 2.0))
+		if Input.is_action_just_released("LeftMouse"):
+			Input.set_custom_mouse_cursor(set_scaled_cursor(non_dragging_cursor, cursor_size), Input.CURSOR_ARROW, Vector2(cursor_size / 2.0, cursor_size / 2.0))
