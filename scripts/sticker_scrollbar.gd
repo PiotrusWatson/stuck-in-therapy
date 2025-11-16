d4b00b59e48017fd7bd11ee0b63377c26858e90e
Aral Acikalin <aralacikalin@hotmail.com>
2025-11-16 14:39:20 +0200

    make scrollbar ticker and change color

--- scripts/sticker_scrollbar.gd
+++ scripts/sticker_scrollbar.gd
@@ -4,9 +4,15 @@ extends VBoxContainer
 @export var stciker_resource: Array[StickerData]
 @export var sticker_prefab: PackedScene
 @export var sticker_daddy: Node
+@export var scrollbar_container: ScrollContainer
+@export var scrollbar_thickness_margin: int = 18
 
 
 func _ready() -> void:
+
+	custom_minimum_size.x = scrollbar_container.size.x - scrollbar_thickness_margin
+	size_flags_horizontal = 0
+
 	for sticker: StickerData in stciker_resource:
 		var sticker_instance = sticker_prefab.instantiate() as DraggableSticker
 		sticker_instance.texture =(sticker as StickerData).sticker_texture
